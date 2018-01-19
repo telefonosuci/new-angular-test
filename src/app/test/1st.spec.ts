273644f87c71f238d44dcdfeb5a8b7d41ba2b839
Enrico <telefonosuci@gmail.com>
2018-01-19 13:14:21 +0100

    Added some Jasmine unit test

---
+++ src/app/test/1st.spec.ts
@@ -0,0 +1,15 @@
+
+
+describe('1st tests', () => {
+    it('true is true', () => expect(true).toBe(true));
+});
+
+describe('personal tests', () => {
+    let a=1;
+    it('a is 1', () => expect(a).toBe(1));
+});
+
+describe('second personal tests', () => {
+    let a=1;
+    it('a is 2', () => expect(a).toBe(2));
+});
